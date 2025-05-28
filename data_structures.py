998350708e68a745b7fd8757039450bb98044575
allnrvao <aracuna@uamv.edu.ni>
2025-05-28 08:08:07 -0600

    commit allan

---
+++ data_structures.py
@@ -0,0 +1,37 @@
+class Stack:
+    def __init__(self):
+        self.items = []
+    
+    def push(self, item):
+        self.items.append(item)
+    
+    def pop(self):
+        if not self.is_empty():
+            return self.items.pop()
+        return None
+    
+    def is_empty(self):
+        return len(self.items) == 0
+    
+    def peek(self):
+        if not self.is_empty():
+            return self.items[-1]
+        return None
+
+class Queue:
+    def __init__(self):
+        self.items = []
+    
+    def enqueue(self, item):
+        self.items.append(item)
+    
+    def dequeue(self):
+        if not self.is_empty():
+            return self.items.pop(0)
+        return None
+    
+    def is_empty(self):
+        return len(self.items) == 0
+    
+    def display(self):
+        return self.items
